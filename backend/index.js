ef1aa01890a936ddad8733c787a8f171174e2816
amielonen <81672251+amielonen@users.noreply.github.com>
2022-02-23 13:40:04 +0200

    web scraping goes further

--- backend/index.js
+++ backend/index.js
@@ -14,15 +14,14 @@ async function run() {
 
   await page.goto("https://www.8a.nu/user/mani-hubaer/sportclimbing");
 
-await page.waitForTimeout(4000);
-const linkHandlers = await page.$x("//a[contains(text(), 'Log in')]");
+  await page.waitForTimeout(2000);
+  const linkHandlers = await page.$x("//a[contains(text(), 'Log in')]");
 
-if (linkHandlers.length > 0) {
-  await linkHandlers[0].click();
-} else {
-  throw new Error("Link not found");
-}
-
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click();
+  } else {
+    throw new Error("Link not found");
+  }
 
   const usernameSelector = "#username";
   const pwSelector = "#password";
@@ -40,40 +39,28 @@ if (linkHandlers.length > 0) {
 
   await page.click(buttonSelector);
 
-}
+  // dropdown
+  const dd =
+    "#__layout > div > div > div > div > div.column-center.cell.auto > div > div.column-content-left.cell.auto > div > div > div.content-child > div.user-stats > select";
+  await page.waitForSelector(dd);
+  await page.waitForTimeout(1500);
+  await page.click(dd);
 
-//mene ensin käyttäjän sivulle ja tee login SIVUVALIKOSTA"!
-async function run2() {
-  const br = await puppeteer.launch({ headless: false });
-  const page = await br.newPage();
+  // select all time
+  
+  await page.waitForTimeout(10);
+  await page.keyboard.press("ArrowDown");
+  await page.keyboard.press("Enter");
+//väärä dropdown? valitaan statistiikka eikä reitit
 
-  await page.goto("https://www.8a.nu/login");
 
-  const usernameSelector = "#username";
-  const pwSelector = "#password";
-  const buttonSelector = "#kc-login";
+//seuraavaksi listasta pitää kaivaa kaikki info reiteistä
+//- greidi ei jokaiselle reitille, vaan greidin alla siihen kuuluvat nousut
+//  - greidi ja sen nousut kuuluvat samaan table bodyyn, eli voidaan käydä niitä yksi kerrallaan läpi
+//    - ekalla rivi th, jossa tekstinä greidi
+//        - välissä 1 turha rivi, seuraavalla rivillä muu data: reitin nimi, maa, cragi - kommentti - ascent type (FA?), tähdet/rating, DATE
+  }
 
-  const user = "hoyanes954@submic.com";
-  const pw = "ebin1234";
 
-  await page.click(usernameSelector);
-  await page.keyboard.type(user);
-
-  await page.click(pwSelector);
-  await page.keyboard.type(pw);
-
-  await page.click(buttonSelector);
-
-  //await page.waitForNavigation();
-
-  await page.goto("https://www.8a.nu/user/mani-hubaer/sportclimbing");
-
-  const searchSelector =
-    "#__layout > div > div > header > div > div.page-navigation.grid-x > div.cell.shrink.small-4.tablet-auto.text-right > nav.grid-x.top-navigation-mobile > div.cell.auto > div > i";
-
-  page.click(searchSelector);
-
-  page.keyboard.type("mani hubär");
-}
 
 run();
